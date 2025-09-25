989797ec571869afcb16d0e47edd38c3702ac1f6
Abhishek9678 <abhishek@houseofevolve.in>
2025-09-25 18:39:08 +0530

    logo in footer alignment

--- components/footer.tsx
+++ components/footer.tsx
@@ -54,7 +54,7 @@ export default function Footer() {
           {/* Brand */}
           <div className="col-span-1 lg:col-span-2">
             <div className="flex items-center space-x-2 mb-4">
-              <Image src="/hoeee.png" alt="HOE" width={200} height={200} />
+              <Image src="/hoeee.png" alt="HOE" width={200} height={200} className="-ml-19" />
               
             </div>
             <p className="text-sm text-slate-300 mb-6">Quality gear and apparel curated from top brands.</p>
